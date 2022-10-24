25aa35e6c558f6b14de102c690080d4046d117bc
theophanee <caraux.t@gmail.com>
2022-10-24 17:23:26 +0200

    prb make

--- Arete.cpp
+++ Arete.cpp
@@ -1,27 +1,38 @@
-#ifndef _ARETE
-#define _ARETE
-#include"Sommet.hpp"
+#include "Arete.hpp"
 #include<iostream>
 #include<string>
 
 using namespace std;
 
-class Arete{
-    private:
-        Sommet *gauche, *droite;
-        int poids;
-    public:
-        Arete(string etiquette1, string etiquette2, int p);
-        Arete(const Arete &a);
-        Arete(Sommet *g, Sommet *d, int p);
-        virtual ~Arete();
-        Sommet* getGauche();
-        Sommet* getDroite();
-        int getPoids();
-        void setPoids(int p);
-        
-};
+Arete::Arete(string etiquette1, string etiquette2, int p) : gauche{new Sommet{etiquette1}}, droite{new Sommet{etiquette2}}, poids{p} {
+    cout << "Construction de " << *this;
+}
 
-ostream& operator <<(ostream& out, Arete &a) ;
+Arete::Arete(const Arete &a) : poids{a.poids} {
+    gauche = a.gauche;
+    droite = a.droite;
+    cout << "Copie de "<< *this<<endl;
+}
 
-#endif
+Arete::Arete(Sommet *g, Sommet *d, int p) : poids{p} {
+    gauche = g;
+    cout<<"coco "<<g<<endl;
+
+    droite = d;
+    cout << "Construction de " << *this ;
+}
+
+Arete::~Arete(){
+    cout<<"Destruction de l'arete "<< *this<<endl;
+}
+
+Sommet* Arete::getDroite(){ return droite ;}
+Sommet* Arete::getGauche() { return  gauche; }
+int Arete::getPoids(){ return poids; }  
+void Arete::setPoids(int p){ poids = p ;}
+
+ostream& operator<<( ostream& out , Arete &a){
+    out<<"Arete("<< a.getPoids() <<"):\nGauche-> "<< (a.getGauche()) << " Droite-> " << (a.getDroite()) <<endl ;
+    return out ;
+}
+
